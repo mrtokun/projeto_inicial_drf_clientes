5bf56d0d69390cf47e853e59ff5b4488b912402f
mrtokun <tokunaga.paulo3@gmail.com>
2024-07-04 16:00:46 -0300

    Validações de Campos e Libs de Apoio

--- clientes/serializers.py
+++ clientes/serializers.py
@@ -1,24 +1,21 @@
 from rest_framework import serializers
 from clientes.models import Cliente
+from clientes.validators import *
 
 class ClienteSerializer(serializers.ModelSerializer):
     class Meta:
         model = Cliente
         fields = '__all__'
-    def validate_cpf(self, cpf):
-        if len(cpf) != 11:
-            raise serializers.ValidationError("O CPF deve ter 11 caracteres")
-        return cpf        
-    def validate_nome(self, nome):
-        if not nome.isalpha():
-            raise serializers.ValidationError("O nome deve possuir apenas letras.")
-        return nome
-    def validate_rg(selg, rg):
-        if len(rg) != 9:
-            raise serializers.ValidationError("O RG deve ter 9 caracteres")
-        return rg
-    def validate_celular(self, celular):
-        if len(celular) < 11 or len(celular) > 14:
-            raise serializers.ValidationError("O celular deve ter de 11 a 14 caracteres")
-        return celular
+    def validate(self, data):
+        if not cpf_valido(data['cpf']):
+            raise serializers.ValidationError({'cpf': "Número de CPF inválido."})
+        if not nome_valido(data['nome']):
+            raise serializers.ValidationError({'nome':"O nome deve possuir apenas letras."})
+        if not rg_valido(data['rg']):
+            raise serializers.ValidationError({'rg': "O RG deve ter 9 caracteres"})
+        # if not celular_valido(data['celular']):
+        #     raise serializers.ValidationError({'celular': "O celular deve ter de 11 a 14 caracteres"})
+        if not celular_valido(data['celular']):
+            raise serializers.ValidationError({'celular':"O número de celular deve atender o seguinte padrão: (xx) xxxxx-xxxx"})
+        return data
     
