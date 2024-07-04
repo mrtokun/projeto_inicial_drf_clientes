f24d1c740c65c8cb7fa1fc5608f329d3a77198cf
mrtokun <tokunaga.paulo3@gmail.com>
2024-07-03 23:56:14 -0300

    Validação no serializer

--- clientes/serializers.py
+++ clientes/serializers.py
@@ -5,4 +5,20 @@ class ClienteSerializer(serializers.ModelSerializer):
     class Meta:
         model = Cliente
         fields = '__all__'
+    def validate_cpf(self, cpf):
+        if len(cpf) != 11:
+            raise serializers.ValidationError("O CPF deve ter 11 caracteres")
+        return cpf        
+    def validate_nome(self, nome):
+        if not nome.isalpha():
+            raise serializers.ValidationError("O nome deve possuir apenas letras.")
+        return nome
+    def validate_rg(selg, rg):
+        if len(rg) != 9:
+            raise serializers.ValidationError("O RG deve ter 9 caracteres")
+        return rg
+    def validate_celular(self, celular):
+        if len(celular) < 11 or len(celular) > 14:
+            raise serializers.ValidationError("O celular deve ter de 11 a 14 caracteres")
+        return celular
     
